9d4b4944e11b130860dcf94a2a7c0352f7bb7f7b
julia sheremet <juliajessicasher@gmail.com>
2018-03-23 17:14:34 -0700

    name should appread on form

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 //frontend logic
 $(document).ready(function(){
-
   $("form#coding-form").submit(function(event){
     event.preventDefault();
 
@@ -10,21 +9,9 @@ $(document).ready(function(){
     var question3 = parseInt($("#input-q3").val());
     var question4 = parseInt($("#input-q4").val());
     var question5 = parseInt($("#input-q5").val());
-    var name = "Dear, " + $("#name").val() + ",";
-
+    var userName = $("input#name").val();
 
     var results = question1 + question2 + question3 + question4 + question5;
-
-    // $("#coding-form button").click(function(e){
-    //   if($(this).attr("value")=="name-btn"){
-    //     alert('hi');
-    //     $(".post-card").show();
-    //   }
-    //
-    // });
-
-
-
 
 //backend logic
     if (results <= 7) {
@@ -32,10 +19,12 @@ $(document).ready(function(){
       // $(".c-sharp-result").show();
       // console.log(results);
       // // alert('hi');
-
+      $(".user-name").text(userName);
       $(".user-output").prepend($(".c-sharp-result").show());
-      $(".post-card-name").text(name);
-      $(".post-card").show();
+      // $(".user-name").text(name);
+
+      // alert('hi');
+      // debugger;
 
     } else if (results >= 8 && results <=10) {
       // $prompt(name + "you should take...");
@@ -43,19 +32,14 @@ $(document).ready(function(){
       // $(".react-css-result").show();
       // console.log(results);
       $(".user-output").prepend($(".react-css-result").show());
-      $(".post-card-name").text(name);
-      $(".post-card").show();
-
 
     } else if (results >= 11 && results <= 15) {
       // $("#user-output").hide();
       // $(".java-android-result").show();
       // console.log(results);
       $(".user-output").prepend($(".java-android-result").show());
-      $(".post-card-name").text(name);
-      $(".post-card").show();
-
     }
+
   });
 
 });
